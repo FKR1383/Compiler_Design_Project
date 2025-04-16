1ac42d5d5e5f9ce2850a8fc4dc176534d46ff688
FKR1383 <farzamprogrammer@gmail.com>
2025-04-16 04:26:23 +0330

    first try

---
+++ compiler.py
@@ -0,0 +1,161 @@
+# compiler.py
+# Full Name: Farzam Koohi-Ronaghi
+# Student ID: [Insert your student number here]
+# References: Kenneth C. Louden, Compiler Construction: Principles and Practice
+
+import re
+
+class Scanner:
+    def __init__(self, input_path='input.txt'):
+        self.input_path = input_path
+        self.tokens_output_path = 'tokens.txt'
+        self.errors_output_path = 'lexical_errors.txt'
+        self.symbol_table_output_path = 'symbol_table.txt'
+
+        self.line_number = 0
+        self.symbol_table = {}
+        self.symbol_table_index = 1
+        self.tokens_by_line = {}
+        self.errors_by_line = {}
+
+        self.current_line = ''
+        self.position = 0
+
+        self.keywords = {'if', 'else', 'void', 'int', 'while', 'break', 'return'}
+        self.symbols = {';', ':', ',', '[', ']', '(', ')', '{', '}', '+', '-', '*', '/', '=', '<'}
+
+    def read_file(self):
+        with open(self.input_path, encoding='utf-8') as file:
+            return file.readlines()
+
+    def save_output_files(self):
+        self.write_tokens()
+        self.write_errors()
+        self.write_symbol_table()
+
+    def write_tokens(self):
+        with open(self.tokens_output_path, 'w', encoding='utf-8') as f:
+            for line in sorted(self.tokens_by_line.keys()):
+                token_strs = ' '.join([f'({ttype}, {tval})' for (ttype, tval) in self.tokens_by_line[line]])
+                f.write(f'{line} {token_strs}\n')
+
+    def write_errors(self):
+        with open(self.errors_output_path, 'w', encoding='utf-8') as f:
+            if not self.errors_by_line:
+                f.write('There is no lexical error.\n')
+            else:
+                for line in sorted(self.errors_by_line.keys()):
+                    for err in self.errors_by_line[line]:
+                        f.write(f'{line} {err}\n')
+
+    def write_symbol_table(self):
+        with open(self.symbol_table_output_path, 'w', encoding='utf-8') as f:
+            for idx, lexeme in enumerate(self.symbol_table.keys(), start=1):
+                f.write(f'{idx}.	{lexeme}\n')
+
+    def scan(self):
+        lines = self.read_file()
+        while_comment = False
+        comment_start_line = 0
+        comment_buffer = ''
+        for lineno, line in enumerate(lines, start=1):
+            self.line_number = lineno
+            self.current_line = line
+            self.position = 0
+            while self.position < len(self.current_line):
+                if while_comment:
+                    if self.current_line[self.position:self.position+2] == '*/':
+                        while_comment = False
+                        self.position += 2
+                        continue
+                    else:
+                        comment_buffer += self.current_line[self.position]
+                        self.position += 1
+                        continue
+                token = self.get_next_token()
+                if token:
+                    ttype, tval = token
+                    if ttype == 'ERROR':
+                        self.errors_by_line.setdefault(lineno, []).append(f'({tval[0]}, {tval[1]})')
+                    elif ttype not in ['WHITESPACE', 'COMMENT']:
+                        self.tokens_by_line.setdefault(lineno, []).append(token)
+
+            if while_comment:
+                comment_buffer += '\n'
+
+        if while_comment:
+            snippet = comment_buffer[:7] + '...' if len(comment_buffer) > 7 else comment_buffer
+            self.errors_by_line.setdefault(comment_start_line, []).append(f'({snippet}, Unclosed comment)')
+
+    def get_next_token(self):
+        if self.position >= len(self.current_line):
+            return None
+
+        ch = self.current_line[self.position]
+
+        if ch.isspace():
+            self.position += 1
+            return ('WHITESPACE', ch)
+
+        # Comments
+        if self.current_line[self.position:self.position+2] == '/*':
+            end_pos = self.current_line.find('*/', self.position+2)
+            if end_pos != -1:
+                self.position = end_pos + 2
+                return ('COMMENT', '/*...*/')
+            else:
+                self.position += 2
+                return ('ERROR', ('/*', 'Unclosed comment'))
+
+        if self.current_line[self.position:self.position+2] == '*/':
+            self.position += 2
+            return ('ERROR', ('*/', 'Unmatched comment'))
+
+        # Two-char symbol: ==
+        if self.current_line[self.position:self.position+2] == '==':
+            self.position += 2
+            return ('SYMBOL', '==')
+
+        # SYMBOL
+        if ch in self.symbols:
+            self.position += 1
+            return ('SYMBOL', ch)
+
+        # NUM
+        if ch.isdigit():
+            start = self.position
+            while self.position < len(self.current_line) and self.current_line[self.position].isdigit():
+                self.position += 1
+            if self.position < len(self.current_line) and self.current_line[self.position].isalpha():
+                error_start = self.position
+                while self.position < len(self.current_line) and self.current_line[self.position].isalnum():
+                    self.position += 1
+                return ('ERROR', (self.current_line[start:self.position], 'Invalid number'))
+            return ('NUM', self.current_line[start:self.position])
+
+        # ID or Keyword
+        if ch.isalpha():
+            start = self.position
+            while self.position < len(self.current_line) and self.current_line[self.position].isalnum():
+                self.position += 1
+            lexeme = self.current_line[start:self.position]
+            if lexeme in self.keywords:
+                if lexeme not in self.symbol_table:
+                    self.symbol_table[lexeme] = self.symbol_table_index
+                    self.symbol_table_index += 1
+                return ('KEYWORD', lexeme)
+            else:
+                if lexeme not in self.symbol_table:
+                    self.symbol_table[lexeme] = self.symbol_table_index
+                    self.symbol_table_index += 1
+                return ('ID', lexeme)
+
+        # Invalid input
+        self.position += 1
+        return ('ERROR', (ch, 'Invalid input'))
+
+
+if __name__ == '__main__':
+    scanner = Scanner()
+    scanner.scan()
+    scanner.save_output_files()
